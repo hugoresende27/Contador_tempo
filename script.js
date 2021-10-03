23561156b939e00c4bc51dff1fa4f3164268d498
hugoresende27 <Hugoresende27@gmail.com>
2021-10-03 23:09:24 +0100

    dom

--- script.js
+++ script.js
@@ -154,3 +154,57 @@ submitBtn.addEventListener("click", () => {
         }
     }
 });
+
+///////////////////////////////////////////////
+const meals = document.getElementById('meals');
+getRandomMeal();
+
+async function getRandomMeal()
+{
+    const resp=await fetch('http://www.themealdb.com/api/json/v1/1/random.php');
+    const respDados = await resp.json();
+    const randomMeal = respDados.meals[0];
+    console.log(randomMeal);
+
+    addMeal(randomMeal,true);
+}
+
+async function getMealsById(id)
+{
+    const meal=await fetch ('http://www.themealdb.com/api/json/v1/1/lookup.php?i='+id);
+}
+
+async function getMealsBySearch(term)
+{
+    const meals=await fetch ('http://www.themealdb.com/api/json/v1/1/search.php?s='+term);
+}
+
+function addMeal(mealData,random=false)
+{
+    const meal = document.createElement('div');
+    meal.classList.add('meal');
+    meal.innerHTML=` 
+            <div class="refeicao-header">
+                ${random?`<span class="aleatorio"> Legumes Cenouras </span>         `:''}
+
+                <img src="${mealData.strMealThumb}" alt="${mealData.strMeal}">
+            </div>
+            
+            <div class="refeicao-body">
+                <h4>${mealData.strMeal}</h4>
+                <button class="fav-btn">                 <!--i.fas.fa-heart   shortcut para botao coracao-->
+                    <i class="fas fa-heart"></i>              
+                </button>
+            </div>
+    
+    
+    `;
+
+    const btn = meal.querySelector(".refeicao-body .fav-btn");
+    btn.addEventListener("click",(e) => {
+        btn.classList.toggle("active");
+    });
+
+    meals.appendChild(meal);
+}
+
